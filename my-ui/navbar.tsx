e5bce4e186af932698aedd3504b7d5571e6e4882
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-03-16 01:37:18 +0530

    feat: update state initialization and enhance navbar links
    Initialize `jsonData` state with an empty string to prevent null 
    issues. Add Poppins font import for improved typography. Update 
    navbar links to open in a new tab for better user experience. 
    Enhance README with GitHub badges for better project visibility.

--- my-ui/navbar.tsx
+++ my-ui/navbar.tsx
@@ -15,23 +15,20 @@ import Link from "next/link";
 export function Navbar() {
   return (
     <div className="container">
-      <div className="item active" style={{ marginRight: "4px !important" }}>
-        <Home size={16} />
-      </div>
+        <a href="/" className="item active" style={{ marginRight: "4px !important" }}>
+          <Home size={16} />
+        </a>
       <div className="seperator"></div>
-      <a href="/how"
-        className="item"
-        style={{ marginRight: "6px !important", marginLeft: "4px !important" }}
-      >
+      <a href="https://github.com/divyanshudhruv/datadiver-ai/blob/main/README.md" target="_blank" rel="noopener noreferrer" className="item" style={{ marginRight: "6px !important", marginLeft: "4px !important" }}>
         <SquareActivityIcon size={16} />
         <div className="text">How</div>
       </a>
       
-      <a href="https://github.com/divyanshudhruv/datadiver-ai/blob/main/examples" className="item" style={{ marginRight: "6px !important" }}>
+      <a href="https://github.com/divyanshudhruv/datadiver-ai/blob/main/examples" target="_blank" rel="noopener noreferrer" className="item" style={{ marginRight: "6px !important" }}>
         <BookAudio size={15} />
         <div className="text">Examples</div>{" "}
       </a>
-        <a href="https://github.com/divyanshudhruv/datadiver-ai" className="item">
+        <a href="https://github.com/divyanshudhruv/datadiver-ai" target="_blank" rel="noopener noreferrer" className="item">
           <ContainerIcon size={15} />
           <div className="text">Github</div>{" "}
         </a>
